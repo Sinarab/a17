a67950a2e3a9de22830e0032b023b32c1f443c72
SinaraB <sinara_b@outlook.com>
2024-03-18 11:06:13 -0300

    fix page not found path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { ShowcaseComponent } from './showcase/showcase.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
